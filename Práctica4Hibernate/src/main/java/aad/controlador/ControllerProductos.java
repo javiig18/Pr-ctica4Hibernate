e7df14937abc69e930af1ba7eacecb53215508e0
miguel <miguel@miguel-VirtualBox>
2019-11-27 12:51:35 +0100

    Controlador paquete creado

---
+++ Práctica4Hibernate/src/main/java/aad/controlador/ControllerProductos.java
@@ -0,0 +1,17 @@
+package aad.controlador;
+
+import aad.modelo.Producto;
+import aad.modelo.ProductoDAO;
+import aad.modelo.ProductoJDBC;
+
+public class ControllerProductos {
+ProductoDAO productoController = new ProductoJDBC();
+
+
+public Producto crearProducto(String nombre,float precio,int stock) {
+	
+	return productoController.crearProducto(nombre, precio, stock);
+}
+
+
+}
